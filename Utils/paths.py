2b490483acff7ca4cd17b4eda9df334003f60967
Andrew82106 <qq281512383@163.com>
2023-11-10 21:51:20 +0800

    新电脑上的commit 1st

--- Utils/paths.py
+++ Utils/paths.py
@@ -1,6 +1,6 @@
 import os
 
-rootPth = "/root/autodl-tmp/Chinese-Slang-Recognition-with-MECT-Model"
+rootPth = "B:\Chinese-Slang-Recognition-with-MECT-Model"
 embeddings = os.path.join(rootPth, "datasets/embeddings")
 charinfo = os.path.join(rootPth, "datasets/charinfo")
 NER = os.path.join(rootPth, "datasets/NER")
