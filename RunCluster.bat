4e152c5e9f04582fa3e006f0bdbb6960335daff8
Andrew82106 <qq281512383@163.com>
2023-12-22 17:55:50 +0800

    update RunCluster脚本

--- RunCluster.bat
+++ RunCluster.bat
@@ -1,3 +1,5 @@
+python cluster.py --mode clean_function_cache
+python cluster.py --mode clean_model_cache
 python main.py --dataset msra --status generate --device cpu --extra_datasets wiki
 python cluster.py --mode generate
 python cluster.py --mode test
